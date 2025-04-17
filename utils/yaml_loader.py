f59036187cdea0f0e74934d476a211d3a46dc155
LordKaT <lordkat@gmail.com>
2025-04-17 11:59:39 -0400

    Trying to resolve 404

--- utils/yaml_loader.py
+++ utils/yaml_loader.py
@@ -1,7 +1,9 @@
-import js, yaml
+import js, micropip
 from logic.entity import Entity
 
 async def load_entity_from_yaml(path):
+    await micropip.install("pyyaml")
+    import yaml
     res = await js.fetch(path)
     text = await res.text()
     data = yaml.safe_load(text)
