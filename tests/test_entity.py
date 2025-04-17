16480d405e0ce453cae40a983db719745f0986df
LordKaT <lordkat@gmail.com>
2025-04-17 12:34:58 -0400

    Trying to resolve 404

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -3,10 +3,7 @@ from js import console, document
 import asyncio
 
 async def run():
-    console.log("Loading")
     entity = await load_entity_from_yaml("data/entities/test/test_goblin.yaml")
-
-    console.log("Loaded: ", entity)
     
     assert entity.get_stat("hp") == 40
     assert entity.get_stat("max_hp") == 40
@@ -18,4 +15,4 @@ async def run():
     assert entity.set_stat("intelligence", 2) == 2
     assert entity.get_stat("intelligence") == 2
     
-    document.getElementById("output").innerHTML += f"<p>[PASS] {entity.name} loaded and tested: {entity}"
+    document.getElementById("output").innerHTML += f"<p>[PASS] {entity.name} loaded and tested"
