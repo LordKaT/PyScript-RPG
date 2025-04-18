ffcf805258ec8f18f38d29b7dc38cbf8de7357b6
LordKaT <lordkat@gmail.com>
2025-04-17 20:08:27 -0400

    Stats breakout

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -1,24 +1,29 @@
 from utils.yaml_loader import load_entity_from_yaml
 from js import document, console
-import asyncio
+import asyncio, sys, traceback
 
 async def run():
     entity = await load_entity_from_yaml("data/entities/test/test_goblin.yaml")
-    document.getElementById("output").innerHTML += "<p>Test start</p>"
-    
-    assert entity.stats.get("hp") == 40
-    assert entity.stats.get("max_hp") == 40
-    assert entity.stats.get("tp") == 100
-    assert entity.stats.get("max_tp") == 100
-    assert entity.stats.get("strength") == 2
-    assert entity.stats.get("agility") == 5
-    assert entity.stats.modify("agility", 10) == 15
-    assert entity.stats.set("intelligence", 2) == 2
-    assert entity.stats.get("intelligence") == 2
-    assert entity.stats.get("level") == 1
-    assert entity.stats.set("level", 2) == 2
-    assert entity.stats.derived()["tnl"] == 4500
-    assert entity.stats.set("xp", 100) == 100
-    assert entity.stats.derived()["tnl"] == 4400
-
-    document.getElementById("output").innerHTML += f"<p>[PASS] {entity.name} loaded and tested"
+    document.getElementById("output").innerHTML += "<p>[START] Begin...</p>"
+    try:
+        assert entity.stats.get("hp") == 40
+        assert entity.stats.get("max_hp") == 40
+        assert entity.stats.get("tp") == 100
+        assert entity.stats.get("max_tp") == 100
+        assert entity.stats.get("strength") == 2
+        assert entity.stats.get("agility") == 5
+        assert entity.stats.modify("agility", 10) == 15
+        assert entity.stats.set("intelligence", 2) == 2
+        assert entity.stats.get("intelligence") == 2
+        assert entity.stats.get("level") == 1
+        assert entity.stats.set("level", 2) == 2
+        assert entity.stats.derived()["tnl"] == 4500
+        assert entity.stats.set("xp", 100) == 100
+        assert entity.stats.derived()["tnl"] == 4400
+        document.getElementById("output").innerHTML += f"<p>[PASS]</p>"
+    except AssertionError:
+        _, _, tb = sys.exc_info()
+        traceback.print_tb(tb) # Fixed format
+        tb_info = traceback.extract_tb(tb)
+        filename, line, func, text = tb_info[-1]
+        document.getElementById("output").nnerHTML += f"<p>[FAIL] {entity.name} line {line}, {text}</p>"
